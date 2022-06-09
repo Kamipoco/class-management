922a8d0e4808574a760519324fd5c3db489dec91
Lam Nguyen <vanlam020499@gmail.com>
2022-06-09 23:07:15 +0700

    update convert lecturers

--- src/services/convertJsonLecturer.js
+++ src/services/convertJsonLecturer.js
@@ -1,15 +1,78 @@
 import XlsxPopulate from "xlsx-populate";
 
 async function testConvertLecturers(lecturers) {
-  //C1
-  XlsxPopulate.fromFileAsync("./src/template/lecturers.xlsx").then(
-    (workbook) => {
-      // Modify the workbook.
-      const value = workbook.sheet(0).cell("A1").value("test");
-      // Log the value.
-      console.log(value);
-    }
+  let workbook = await XlsxPopulate.fromFileAsync(
+    "./src/template/lecturers.xlsx"
   );
+
+  for (const element in lecturers) {
+    let cells = Number(element) + 2;
+    let sheets = 0;
+
+    //#region Title
+    const titleName = workbook
+      .sheet(sheets)
+      .cell(`A1`)
+      .style("bold", true)
+      .value("lecturer_name");
+    const titleEmail = workbook
+      .sheet(sheets)
+      .cell(`B1`)
+      .style("bold", true)
+      .value("email");
+    const titleBio = workbook
+      .sheet(sheets)
+      .cell(`C1`)
+      .style("bold", true)
+      .value("bio");
+    const titleRole = workbook
+      .sheet(sheets)
+      .cell(`D1`)
+      .style("bold", true)
+      .value("role");
+    const titleCreatedAt = workbook
+      .sheet(sheets)
+      .cell(`E1`)
+      .style("bold", true)
+      .value("createdAt");
+    const titleUpdatedAt = workbook
+      .sheet(sheets)
+      .cell(`F1`)
+      .style("bold", true)
+      .value("updatedAt");
+    //#endregion
+
+    //#region Data
+    const customName = workbook
+      .sheet(sheets)
+      .cell(`A${cells}`)
+      .value(lecturers[element].lecturer_name);
+    const customEmail = workbook
+      .sheet(sheets)
+      .cell(`B${cells}`)
+      .value(lecturers[element].email);
+    const customBio = workbook
+      .sheet(sheets)
+      .cell(`C${cells}`)
+      .value(lecturers[element].bio);
+    const customRole = workbook
+      .sheet(sheets)
+      .cell(`D${cells}`)
+      .value(lecturers[element].role);
+    const customCreatedAt = workbook
+      .sheet(sheets)
+      .cell(`E${cells}`)
+      .value(lecturers[element].createdAt);
+    const customUpdatedAt = workbook
+      .sheet(sheets)
+      .cell(`F${cells}`)
+      .value(lecturers[element].updatedAt);
+    //#endregion
+  }
+
+  return workbook.outputAsync();
+
+  //#region Cach cu
   //C2;
   //   let workbook = await XlsxPopulate.fromBlankAsync();
   //   for (const element in lecturers) {
@@ -76,6 +139,8 @@ async function testConvertLecturers(lecturers) {
   //   }
   //   // return workbook;
   //   return workbook.outputAsync();
+
+  //#endregion
 }
 
 module.exports = {
